58b0c5666708f44d0be91c8c8b4c6aacb0672313
Justin <76699137+Cownex@users.noreply.github.com>
2023-02-03 22:36:57 +0100

    updated base_request

--- src/pylesk_Cownex/utils/base_api.py
+++ src/pylesk_Cownex/utils/base_api.py
@@ -1,12 +1,22 @@
 # Base API
+import requests
 
 import exceptions
+
+
+def format_response(response):
+    """
+    Check and Format header
+    :param response:
+    :return: response
+    """
+    return response
 
 
 class PleskAPIClient(object):
     """"""
 
-    def __int__(self, api_token=None, username=None, password=None, basic_auth=False):
+    def __int__(self, url, api_token=None):
         """
         Create an API Instance.
         :param api_token: Plesk-API Token
@@ -14,6 +24,33 @@ class PleskAPIClient(object):
         :param password:Admin Password
         :param basic_auth: Use Basic auth with Username and Password.
         """
+        if not api_token:
+            raise exceptions.PylesException('No API-Token defined.')
+        self.url = url
         self._api_token = api_token
-        self._username = username
-        self._password = password
+
+    def _api_request(self, endpoint, methode='GET', params=None, data=None, custom_header=None):
+        if not endpoint:
+            raise exceptions.BadRequest('No Endpoint defined.')
+        if not methode:
+            raise exceptions.BadRequest('No Methode defined.')
+        url = self.url + endpoint
+        if custom_header:
+            headers = custom_header
+        else:
+            headers = {
+                'Authorization': f'Bearer {self._api_token}',
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        if methode == 'GET':
+            response = requests.get(url, params=params, data=data, headers=headers)
+        elif methode == 'POST':
+            response = requests.post(url, params=params, data=data, headers=headers)
+        elif methode == 'PUT':
+            response = requests.put(url, params=params, data=data, headers=headers)
+        elif methode == 'DELETE':
+            response = requests.delete(url, params=params, data=data, headers=headers)
+        else:
+            raise exceptions.PylesException('No valid Methode defined.')
+        return format_response(response)
